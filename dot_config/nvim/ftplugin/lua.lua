b7e6f549b9d67d65da3760c882d161ba329023bd
Carlos Marx <me@carlosmarx.com>
2023-06-09 02:39:20 -0300

    Reorganizing Neovim config

--- dot_config/nvim/ftplugin/lua.lua
+++ dot_config/nvim/ftplugin/lua.lua
@@ -1,3 +1,4 @@
-vim.bo.expandtab = true
-vim.bo.shiftwidth = 2
-vim.bo.tabstop = 2
+vim.bo.expandtab = false
+vim.bo.shiftwidth = 4
+vim.bo.tabstop = 4
+vim.b.ale_fixers = { "stylua" }
--- dot_config/nvim/lua/config/mason.lua
+++
@@ -1,9 +0,0 @@
-require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "[INSTALLED]",
-            package_pending = "[PENDING]",
-            package_uninstalled = "[UNINSTALLED]"
-        }
-    }
-})
