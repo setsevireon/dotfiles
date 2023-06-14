26be44dc455c4cd634f89a686a37665deaa7ac2a
Carlos Marx <me@carlosmarx.com>
2023-06-14 19:53:01 -0300

    Adopting LazyNvim

---
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "MunifTanjim/nui.nvim", enabled = false },
+  { "JoosepAlviste/nvim-ts-context-commentstring", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
+  { "ggandor/flit.nvim", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+  { "folke/neoconf.nvim", enabled = false },
+}
