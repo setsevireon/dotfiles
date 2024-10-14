b16d1447281d9c4fc67cb6127c651ad46781c210
Carlos Marx <me@carlosmarx.com>
2024-10-14 09:56:15 -0300

    chore: paying debt

---
+++ dot_config/systemd/user/executable_rclone.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+declare -ra DIRS_TO_SYNC=(
+  "organize"
+  "writing"
+  "resume"
+)
+
+for DIR in "${DIRS_TO_SYNC[@]}"; do
+  rclone bisync proton:"${DIR}" "${HOME}/${DIR}" \
+    --verbose \
+    --metadata \
+    --backup-dir1 proton:deleted \
+    --backup-dir2 "${HOME}/deleted" \
+    --create-empty-src-dirs \
+    --compare size,modtime,checksum \
+    --conflict-resolve newer \
+    --conflict-loser pathname \
+    --conflict-suffix conflict-{DateOnly}-proton,conflict-{DateOnly}-wizardtower \
+    --resilient \
+    --recover \
+    --no-slow-hash \
+    --resync
+done
